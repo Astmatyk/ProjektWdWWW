5f95286788937f0d7a26dfa5ae55b8c4e0d32f96
Astmatyk <chsamuell@protonmail.com>
2025-05-23 19:09:24 +0200

    23 05

--- astracloud/static/register.js
+++ astracloud/static/register.js
@@ -1,12 +1,16 @@
-loginForm.addEventListener("submit", async function(event) {
+registerForm.addEventListener("submit", async function(event) {
     event.preventDefault();
 
     const formLogin = document.getElementById("loginData").value.trim();
+    const formEmail = document.getElementById("email").value.trim();
     const formPassword = document.getElementById("password").value;
+    const formPasswordRepeat = document.getElementById("password1").value;
 
     console.log("Dostałem dane");
     console.log("Login:", formLogin);
+    console.log("E-mail:", formEmail);
     console.log("Password:", formPassword);
+    console.log("Password - repeat:", formPasswordRepeat);
 
 });
 
